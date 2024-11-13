62a3f53bf164b0aae5ece5297ae9036e028e9a5b
Jeanne Cheneby <jeannech@uio.no>
2024-11-13 11:19:09 +0100

    refactor: better error handling

--- obiwow/data_reader_parser.py
+++ obiwow/data_reader_parser.py
@@ -20,30 +20,32 @@ def add_duration_to_time(start_time_str: str, duration_str: str) -> str:
     Returns:
         str: The end time in '%H:%M' format.
     """
-    duration_str = duration_str.strip()
-    start_time = datetime.strptime(start_time_str, '%H:%M')
-    duration_match = re.match(r'(\d+)\s*(min|hours?)', duration_str)
-    if not duration_match:
-        print(f'Invalid duration format: {duration_str} in schedule input file, expected format: <number> <unit> (e.g., 2 hours, 30 min)')
+    try:
+        duration_str = duration_str.strip()
+        start_time = datetime.strptime(start_time_str, '%H:%M')
+        duration_match = re.match(r'(\d+)\s*(min|hours?)', duration_str)
+        if not duration_match:
+            raise ValueError(f'Invalid duration format: {duration_str}. Expected format: <number> min|hours in schedule file')
+
+        duration_value = int(duration_match.group(1))
+        duration_unit = duration_match.group(2)
+
+        if 'hour' in duration_unit:
+            duration = timedelta(hours=duration_value)
+        elif 'min' in duration_unit:
+            duration = timedelta(minutes=duration_value)
+        else:
+            raise ValueError("Invalid duration unit")
+
+        end_time = start_time + duration
+        return end_time.strftime('%H:%M')
+    except Exception as e:
+        print(f"WARNING: in add_duration_to_time: {e}")
         if start_time_str == '9:00':
             return '12:00'
         elif start_time_str == '13:00':
             return '16:00'
         return start_time_str
-
-    duration_value = int(duration_match.group(1))
-    duration_unit = duration_match.group(2)
-
-    if 'hour' in duration_unit:
-        duration = timedelta(hours=duration_value)
-    elif 'min' in duration_unit:
-        duration = timedelta(minutes=duration_value)
-    else:
-        raise ValueError("Invalid duration unit")
-
-    end_time = start_time + duration
-    return end_time.strftime('%H:%M')
-
 
 def get_start_end_time(data: pd.Series, schedule_columns: Dict[str, str]) -> Tuple[str, str]:
     """
@@ -56,33 +58,36 @@ def get_start_end_time(data: pd.Series, schedule_columns: Dict[str, str]) -> Tup
     Returns:
         Tuple[str, str]: The start and end time.
     """
-    workshop_start_time = None
-    workshop_end_time = None
-    if data[schedule_columns['duration_column']] == 'all day':
-        workshop_start_time = '9:00'
-        workshop_end_time = '16:00'
-    elif data[schedule_columns['time_column']] == 'morning':
-        if str(data[schedule_columns['duration_column']]) == 'nan':
+    try:
+        workshop_start_time = None
+        workshop_end_time = None
+        if data[schedule_columns['duration_column']] == 'all day':
             workshop_start_time = '9:00'
-            workshop_end_time = '12:00'
-        elif str(data[schedule_columns['duration_column']]) == 'half a day':
-            workshop_start_time = '9:00'
-            workshop_end_time = '12:00'
-        else:
-            workshop_start_time = '9:00'
-            workshop_end_time = add_duration_to_time('9:00', str(data[schedule_columns['duration_column']]))
-    elif data[schedule_columns['time_column']] == 'afternoon':
-        if str(data[schedule_columns['duration_column']]) == 'nan':
-            workshop_start_time = '13:00'
             workshop_end_time = '16:00'
-        elif str(data[schedule_columns['duration_column']]) == 'half a day':
-            workshop_start_time = '13:00'
-            workshop_end_time = '16:00'
-        else:
-            workshop_start_time = '13:00'
-            workshop_end_time = add_duration_to_time('13:00', str(data[schedule_columns['duration_column']]))
-
-    return workshop_start_time, workshop_end_time
+        elif data[schedule_columns['time_column']] == 'morning':
+            if str(data[schedule_columns['duration_column']]) == 'nan':
+                workshop_start_time = '9:00'
+                workshop_end_time = '12:00'
+            elif str(data[schedule_columns['duration_column']]) == 'half a day':
+                workshop_start_time = '9:00'
+                workshop_end_time = '12:00'
+            else:
+                workshop_start_time = '9:00'
+                workshop_end_time = add_duration_to_time('9:00', str(data[schedule_columns['duration_column']]))
+        elif data[schedule_columns['time_column']] == 'afternoon':
+            if str(data[schedule_columns['duration_column']]) == 'nan':
+                workshop_start_time = '13:00'
+                workshop_end_time = '16:00'
+            elif str(data[schedule_columns['duration_column']]) == 'half a day':
+                workshop_start_time = '13:00'
+                workshop_end_time = '16:00'
+            else:
+                workshop_start_time = '13:00'
+                workshop_end_time = add_duration_to_time('13:00', str(data[schedule_columns['duration_column']]))
+        return workshop_start_time, workshop_end_time
+    except Exception as e:
+        print(f"Error in get_start_end_time: {e}")
+        return None, None
 
 
 def add_start_end_time_to_schedule(schedule_df: pd.DataFrame, schedule_columns: Dict[str, str]) -> pd.DataFrame:
@@ -96,14 +101,17 @@ def add_start_end_time_to_schedule(schedule_df: pd.DataFrame, schedule_columns: 
     Returns:
         pd.DataFrame: The updated schedule DataFrame with start and end times.
     """
-    schedule_df[schedule_columns['start_time_column']] = None
-    schedule_df[schedule_columns['end_time_column']] = None
-    for index, row in schedule_df.iterrows():
-        start_time, end_time = get_start_end_time(row, schedule_columns)
-        schedule_df.at[index, schedule_columns['start_time_column']] = start_time
-        schedule_df.at[index, schedule_columns['end_time_column']] = end_time
-
-    return schedule_df
+    try:
+        schedule_df[schedule_columns['start_time_column']] = None
+        schedule_df[schedule_columns['end_time_column']] = None
+        for index, row in schedule_df.iterrows():
+            start_time, end_time = get_start_end_time(row, schedule_columns)
+            schedule_df.at[index, schedule_columns['start_time_column']] = start_time
+            schedule_df.at[index, schedule_columns['end_time_column']] = end_time
+        return schedule_df
+    except Exception as e:
+        print(f"Error in add_start_end_time_to_schedule: {e}")
+        return schedule_df
 
 
 def parse_yaml(path_conf: str) -> Dict[str, Any]:
@@ -126,7 +134,9 @@ def parse_yaml(path_conf: str) -> Dict[str, Any]:
     except FileNotFoundError:
         print(f"Configuration file {path_conf} not found.")
         dict_conf = None
-
+    except Exception as e:
+        print(f"Error in parse_yaml: {e}")
+        dict_conf = None
     return dict_conf
 
 
@@ -146,6 +156,9 @@ def parse_csv_to_pandas(csv_content: str, delimiter: str) -> pd.DataFrame:
     except FileNotFoundError:
         print(f"File {csv_content} not found.")
         df = None
+    except Exception as e:
+        print(f"Error in parse_csv_to_pandas: {e}")
+        df = None
     return df
 
 
@@ -164,11 +177,15 @@ def merge_submission_schedule(submission_df: pd.DataFrame, schedule_df: pd.DataF
     Returns:
         pd.DataFrame: The merged DataFrame.
     """
-    merged_df = pd.merge(submission_df, schedule_df,
-                         left_on=nettskjema_columns['title_column'],
-                         right_on=schedule_columns['title_column'],
-                         how='left')
-    return merged_df
+    try:
+        merged_df = pd.merge(submission_df, schedule_df,
+                             left_on=nettskjema_columns['title_column'],
+                             right_on=schedule_columns['title_column'],
+                             how='left')
+        return merged_df
+    except Exception as e:
+        print(f"Error in merge_submission_schedule: {e}")
+        return pd.DataFrame()
 
 
 def annotate_networking_event(df: pd.DataFrame, schedule_columns: Dict[str, str]) -> pd.DataFrame:
@@ -182,9 +199,13 @@ def annotate_networking_event(df: pd.DataFrame, schedule_columns: Dict[str, str]
     Returns:
         pd.DataFrame: The annotated DataFrame.
     """
-    df[schedule_columns['networking_event_column']] = df[schedule_columns['title_column']].str.startswith(
-        "Networking event")
-    return df
+    try:
+        df[schedule_columns['networking_event_column']] = df[schedule_columns['title_column']].str.startswith(
+            "Networking event")
+        return df
+    except Exception as e:
+        print(f"Error in annotate_networking_event: {e}")
+        return df
 
 
 def generate_ical_content(row: pd.Series, schedule_columns: Dict[str, str], rooms: Dict[str, Dict[str, str]],
@@ -201,27 +222,30 @@ def generate_ical_content(row: pd.Series, schedule_columns: Dict[str, str], room
     Returns:
         str: The iCalendar content.
     """
-    datetime_start = datetime.strptime(
-        row[schedule_columns['date_column']] + '-' + row[schedule_columns['start_time_column']], '%d.%m.%y-%H:%M')
-    datetime_end = datetime.strptime(
-        row[schedule_columns['date_column']] + '-' + row[schedule_columns['end_time_column']], '%d.%m.%y-%H:%M')
+    try:
+        datetime_start = datetime.strptime(
+            row[schedule_columns['date_column']] + '-' + row[schedule_columns['start_time_column']], '%d.%m.%y-%H:%M')
+        datetime_end = datetime.strptime(
+            row[schedule_columns['date_column']] + '-' + row[schedule_columns['end_time_column']], '%d.%m.%y-%H:%M')
 
-    ical_start = datetime_start.strftime("%Y%m%d") + "T" + datetime_start.strftime("%H%M%S")
-    ical_end = datetime_start.strftime("%Y%m%d") + "T" + datetime_end.strftime("%H%M%S")
-    workshop_title = row[schedule_columns['title_column']]
-    room_name = row[schedule_columns['room_column']]
-    room_url = rooms[room_name]['url'] if room_name in rooms else None
-    event_name = yearly['event_name']
+        ical_start = datetime_start.strftime("%Y%m%d") + "T" + datetime_start.strftime("%H%M%S")
+        ical_end = datetime_start.strftime("%Y%m%d") + "T" + datetime_end.strftime("%H%M%S")
+        workshop_title = row[schedule_columns['title_column']]
+        room_name = row[schedule_columns['room_column']]
+        room_url = rooms[room_name]['url'] if room_name in rooms else None
+        event_name = yearly['event_name']
 
-    ics_template = Template(filename='template/invite.ics')
-    ics_content = ics_template.render(ical_start=ical_start,
-                                      ical_end=ical_end,
-                                      workshop_title=workshop_title,
-                                      room_name=room_name,
-                                      room_url=room_url,
-                                      event_name=event_name)
-
-    return ics_content
+        ics_template = Template(filename='template/invite.ics')
+        ics_content = ics_template.render(ical_start=ical_start,
+                                          ical_end=ical_end,
+                                          workshop_title=workshop_title,
+                                          room_name=room_name,
+                                          room_url=room_url,
+                                          event_name=event_name)
+        return ics_content
+    except Exception as e:
+        print(f"Error in generate_ical_content: {e}")
+        return ""
 
 
 def write_ical_files(df: pd.DataFrame, outdir_ics: str, schedule_columns: Dict[str, str],
@@ -236,16 +260,19 @@ def write_ical_files(df: pd.DataFrame, outdir_ics: str, schedule_columns: Dict[s
         rooms (Dict[str, Dict[str, str]]): The room information.
         yearly (Dict[str, str]): The yearly configuration values.
     """
-    Path(outdir_ics).mkdir(parents=True, exist_ok=True)
-    for ics_file in Path(outdir_ics).glob('*.ics'):
-        if ics_file.is_file():
-            ics_file.unlink()
+    try:
+        Path(outdir_ics).mkdir(parents=True, exist_ok=True)
+        for ics_file in Path(outdir_ics).glob('*.ics'):
+            if ics_file.is_file():
+                ics_file.unlink()
 
-    for _, row in df.iterrows():
-        ics_content = generate_ical_content(row, schedule_columns, rooms, yearly)
-        outpath_ics = os.path.join(outdir_ics, str(row[schedule_columns['id_column']]) + ".ics")
-        with open(outpath_ics, 'w') as file:
-            file.write(ics_content)
+        for _, row in df.iterrows():
+            ics_content = generate_ical_content(row, schedule_columns, rooms, yearly)
+            outpath_ics = os.path.join(outdir_ics, str(row[schedule_columns['id_column']]) + ".ics")
+            with open(outpath_ics, 'w') as file:
+                file.write(ics_content)
+    except Exception as e:
+        print(f"Error in write_ical_files: {e}")
 
 
 def write_schedule_json(schedule_df: pd.DataFrame, schedule_columns: dict, output_file: str) -> None:
@@ -257,19 +284,22 @@ def write_schedule_json(schedule_df: pd.DataFrame, schedule_columns: dict, outpu
         schedule_columns (dict): Dictionary mapping column names for the schedule data.
         output_file (str): The path to the output JSON file.
     """
-    schedule_dict = {}
+    try:
+        schedule_dict = {}
 
-    for _, row in schedule_df.iterrows():
-        workshop_id = row[schedule_columns['id_column']]
-        schedule_dict[workshop_id] = {
-            "date": row[schedule_columns['date_column']].strftime('%d.%m.%y'),
-            "room": row[schedule_columns['room_column']],
-            "main_instructor": row[schedule_columns['main_instructor_column']],
-            "helper": row[schedule_columns['helper_instructor_column']],
-            "title": row[schedule_columns['title_column']],
-            "max_attendance": row[schedule_columns['max_attendance']],
-            "timeslot": row[schedule_columns['start_time_column']] + '-' + row[schedule_columns['end_time_column']]
-        }
+        for _, row in schedule_df.iterrows():
+            workshop_id = row[schedule_columns['id_column']]
+            schedule_dict[workshop_id] = {
+                "date": row[schedule_columns['date_column']].strftime('%d.%m.%y'),
+                "room": row[schedule_columns['room_column']],
+                "main_instructor": row[schedule_columns['main_instructor_column']],
+                "helper": row[schedule_columns['helper_instructor_column']],
+                "title": row[schedule_columns['title_column']],
+                "max_attendance": row[schedule_columns['max_attendance']],
+                "timeslot": row[schedule_columns['start_time_column']] + '-' + row[schedule_columns['end_time_column']]
+            }
 
-    with open(output_file, 'w') as json_file:
-        json.dump(schedule_dict, json_file, indent=4)
+        with open(output_file, 'w') as json_file:
+            json.dump(schedule_dict, json_file, indent=4)
+    except Exception as e:
+        print(f"Error in write_schedule_json: {e}")
