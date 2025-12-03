7a81a86e3afe8a80cebbe084ce5894b844af9e57
Lex Nederbragt <lex.nederbragt@ibv.uio.no>
2025-12-03 13:38:44 +0100

    Adjusted for current txt file from nettskjema

--- obiwow.sh
+++ obiwow.sh
@@ -3,10 +3,10 @@
 # Use: sh obiwow.sh data-####-20##-##-##-####-utf.txt
 infile=$1
 echo how many people signed up
-cat $infile | awk -F '\t' '{print $6}' | sort | uniq -c | wc -l
+cat $infile | awk -F ';' '{print $4}' | sort | uniq -c | wc -l
 echo
 echo how many times did someone sign up
-cat $infile | awk -F '\t' '{print $6}' | sort | uniq -c | sort -nr | head
+cat $infile | awk -F ';' '{print $4}' | sort | uniq -c | sort -nr | head
 echo
 echo how many signed up per workshop:
-cat $infile | awk -F '\t' '{print $4}' | sort | uniq -c | sort -nr | awk '{print $2"\t"$1}'
+cat $infile | awk -F ';' '{print $3}' | sort | uniq -c | sort -nr | awk '{print $2"\t"$1}'
